04cf3a0f301216e5435d76bb0ce393047ff5c03f
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-11-05 15:26:16 +0100

    sec bug fixed

--- src/main/java/com/flexisaf/week8/config/WebSecurityConfig.java
+++ src/main/java/com/flexisaf/week8/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())  // Disable CSRF for simplicity (you might want to enable it for production)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/create").permitAll()  // Public endpoint, permit all
+                        .requestMatchers("/api/create/**").permitAll()  // Public endpoint, permit all
                         .anyRequest().authenticated()  // All other endpoints need authentication
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // Add JWT filter before default authentication filter
--- src/main/java/com/flexisaf/week8/dto/UserDto.java
+++ src/main/java/com/flexisaf/week8/dto/UserDto.java
@@ -1,6 +1,9 @@
 package com.flexisaf.week8.dto;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -14,6 +17,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty(message = "please input your firstName")
